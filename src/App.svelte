b5ab66fe7c2a2b5bf982f5e24f10a2d44fb81553
Saltspeddy <walkercoco30@gmail.com>
2023-04-01 12:40:06 +0300

    third commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
+  import Api from './lib/Api.svelte';
   import Counter from './lib/Counter.svelte'
 
   let generatedText
   function readText(){
     generatedText = document.querySelector("#generatedText").innerHTML;
-    console.log(generatedText);
     responsiveVoice.speak(generatedText, "Romanian Female", {onerror: function(){console.log("Error!")}});
   }
 </script>
 
 <main class="min-h-[100vh] w-[100vw] flex flex-col">
-  <div class="h-[100vh] w-[100vw]  flex flex-col justify-center items-center" style="background-image: url(./src/assets/background_wave.png);">
-    <div class="bg-[#FFFFB3] rounded-t-full flex flex-col items-center max-[766px]:rounded-[60px]" style="background-image: url(./assets/background_wave.png);">
+  <div class="h-[100vh] w-[100vw] bg-cover bg-no-repeat shadow-2xl shadow-[#0A5C71] flex flex-col justify-center items-center" style="background-image: url(./src/assets/background_wave.png);">
+    <div class="bg-[#FFFFB3] rounded-t-full flex flex-col items-center max-[766px]:rounded-[60px]">
       <h1 class="text-transparent bg-gradient-to-r from-[#FF0000] to-[#FE8300] font-Montserrat lg:text-[14em] md:text-[10em] text-[4em]" style="-webkit-background-clip: text;">Poetique</h1>
       <input class="w-[50vw] m-10 bg-[#1D263B] text-[#DAE3E5] rounded-[30px] p-3 text-xl font-Montserrat outline-none max-[766px]:w-[60vw]" type="text">
     </div>
@@ -22,5 +22,6 @@
     <button on:click={readText}>read</button>
   </div> 
   <script src="https://code.responsivevoice.org/responsivevoice.js?key=3uuX8akb"></script> 
+  <Api></Api>
 </main>
 
---
+++ src/lib/Api.svelte
@@ -0,0 +1,10 @@
+<script>
+    async function getData(){
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+        const data = await response.json();
+        console.log(data);
+    }
+    getData();
+</script>
+
+<h1>hello</h1>
